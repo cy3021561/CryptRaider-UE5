2811a1eb76f91e8cc173c787248a5eddcd07b9d9
cy3021561 <cyang513@usc.edu>
2023-06-24 17:28:26 -0700

    update

---
+++ Source/CryptRaider/Grabber.cpp
@@ -0,0 +1,104 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Grabber.h"
+#include "Engine/World.h"
+#include "DrawDebugHelpers.h"
+
+
+// Sets default values for this component's properties
+UGrabber::UGrabber()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UGrabber::BeginPlay()
+{
+	Super::BeginPlay();
+
+}
+
+
+// Called every frame
+void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	UPhysicsHandleComponent* physicsHandle = GetPhysicsHandle();
+	if (physicsHandle && physicsHandle->GetGrabbedComponent())
+	{
+		FVector targetLocation = GetComponentLocation() + GetForwardVector() * holdDistance;
+		physicsHandle->SetTargetLocationAndRotation(targetLocation, GetComponentRotation());
+	}
+	
+}
+
+void UGrabber::Release(){
+	UPhysicsHandleComponent* physicsHandle = GetPhysicsHandle();
+	if (physicsHandle && physicsHandle->GetGrabbedComponent())
+	{
+		physicsHandle->GetGrabbedComponent()->WakeAllRigidBodies();
+		AActor* grabbedActor = physicsHandle->GetGrabbedComponent()->GetOwner();
+		grabbedActor->Tags.Remove("Grabbed");
+		physicsHandle->ReleaseComponent();
+	}
+}
+
+void UGrabber::Grab(){
+	UPhysicsHandleComponent* physicsHandle = GetPhysicsHandle();
+	if(physicsHandle == nullptr)
+	{
+		return;
+	}
+
+	FHitResult hitResult;
+	bool hasHit = GetGrabbableInReach(hitResult);	
+
+	if (hasHit)
+	{
+		UPrimitiveComponent* hitComponent = hitResult.GetComponent();
+		hitComponent->SetSimulatePhysics(true);
+		hitComponent->WakeAllRigidBodies();
+		AActor* hitActor = hitResult.GetActor();
+		hitActor->Tags.Add("Grabbed");
+		hitActor->DetachFromActor(FDetachmentTransformRules::KeepWorldTransform);
+		physicsHandle->GrabComponentAtLocationWithRotation(
+			hitComponent,
+			NAME_None,
+			hitResult.ImpactPoint,
+			GetComponentRotation()
+		);
+	}
+
+}
+
+UPhysicsHandleComponent* UGrabber::GetPhysicsHandle() const{
+	UPhysicsHandleComponent* result = GetOwner()->FindComponentByClass<UPhysicsHandleComponent>();
+	if (result == nullptr)
+	{
+		UE_LOG(LogTemp, Error, TEXT("Grabber requires a UPhysicsHandleComponent."));
+	}
+	return result;
+}
+
+bool UGrabber::GetGrabbableInReach(FHitResult& outHitResult) const{
+	FVector start = GetComponentLocation();
+	FVector end = start + GetForwardVector() * maxGrabDistance;
+	DrawDebugLine(GetWorld(), start, end, FColor::Red);
+	DrawDebugSphere(GetWorld(), end, 10, 10, FColor::Blue, false, 5);
+
+	FCollisionShape sphere = FCollisionShape::MakeSphere(grabRadius);
+	return GetWorld()->SweepSingleByChannel
+	(
+		outHitResult, 
+		start, end, 
+		FQuat::Identity, 
+		ECC_GameTraceChannel2,
+		sphere
+	);
+}
---
+++ Source/CryptRaider/TriggerComponent.cpp
@@ -0,0 +1,60 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TriggerComponent.h"
+
+UTriggerComponent::UTriggerComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts
+void UTriggerComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+}
+
+void UTriggerComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+    Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+    AActor* actor = GetAcceptableActor();
+    if (actor != nullptr)
+    {
+        UPrimitiveComponent* component = Cast<UPrimitiveComponent>(actor->GetRootComponent());
+        if (component != nullptr)
+        {
+            component->SetSimulatePhysics(false);
+        }
+        actor->AttachToComponent(this, FAttachmentTransformRules::KeepWorldTransform);
+        mover->SetShouldMove(true);
+    }
+    else
+    {
+        mover->SetShouldMove(false);
+    }
+    
+}
+
+void UTriggerComponent::SetMover(UMover* newMover)
+{
+    mover = newMover;
+}
+
+AActor* UTriggerComponent::GetAcceptableActor() const
+{
+    TArray<AActor*> actors;
+    GetOverlappingActors(actors);
+    for (auto a : actors)
+    {
+        bool hasAcceptableTag = a->ActorHasTag(acceptableActorTag);
+        bool isGrabbed = a->ActorHasTag("Grabbed");  
+        if (hasAcceptableTag && !isGrabbed){
+            return a;
+        }    
+    }
+    return nullptr;
+}
